be4888f7fdd1477b3047357e5964e06931905452
Emerson Melo Martins <emerson.melo@sap.trijay.com.br>
2024-01-12 21:51:33 -0300

    button styles singleton

--- lib/src/pages/splash/splash_page.dart
+++ lib/src/pages/splash/splash_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:fwc_album_app/src/core/styles/button_styles.dart';
 
 class SplashPage extends StatelessWidget {
   const SplashPage({super.key});
@@ -9,7 +10,34 @@ class SplashPage extends StatelessWidget {
       appBar: AppBar(
         title: Text("Splash"),
       ),
-      body: Center(child: Text("Splash")),
+      body: Center(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Text("Splash"),
+          ElevatedButton(
+            child: Text("Botão"),
+            style: ButtonStyles.instance.yellowButton,
+            onPressed: () {},
+          ),
+          OutlinedButton(
+            child: Text("Botão 2"),
+            style: ButtonStyles.instance.yellowOutlineButton,
+            onPressed: () {},
+          ),
+          ElevatedButton(
+            child: Text("Botão"),
+            style: ButtonStyles.instance.primaryButton,
+            onPressed: () {},
+          ),
+          OutlinedButton(
+            child: Text("Botão 2"),
+            style: ButtonStyles.instance.primaryOutlineButton,
+            onPressed: () {},
+          ),
+        ],
+      )),
     );
   }
 }
