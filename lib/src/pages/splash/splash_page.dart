772ed56e8f645c36c555635b7680ea512d2bcec7
Emerson Melo Martins <emerson.melo@sap.trijay.com.br>
2024-01-15 20:25:50 -0300

    messages helper e loader helper

--- lib/src/pages/splash/splash_page.dart
+++ lib/src/pages/splash/splash_page.dart
@@ -1,12 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:fwc_album_app/src/core/ui/helpers/loader.dart';
+import 'package:fwc_album_app/src/core/ui/helpers/messages.dart';
 import 'package:fwc_album_app/src/core/ui/styles/button_styles.dart';
 import 'package:fwc_album_app/src/core/ui/styles/text_styles.dart';
 import 'package:fwc_album_app/src/core/ui/widgets/button.dart';
 import 'package:fwc_album_app/src/core/ui/widgets/rounded_button.dart';
 
-class SplashPage extends StatelessWidget {
+class SplashPage extends StatefulWidget {
   const SplashPage({super.key});
 
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> with Loader<SplashPage>, Messages<SplashPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,11 +29,29 @@ class SplashPage extends StatelessWidget {
             style: context.buttonStyles.yellowButton,
             labelStyle: context.textStyles.textPrimaryFontBold,
             label: "Teste",
-            onPressed: () {},
+            onPressed: () async {
+              showLoader();
+              await Future.delayed(Duration(seconds: 1));
+              hideLoader();
+            },
             height: 300,
           ),
-          Button.primary(label: "Outro", onPressed: () {}, width: 200,),
-          RoundedButton(icon: Icons.abc, onPressed: () {},),
+          Button.primary(
+            label: "Outro",
+            onPressed: () async {
+              showError("Ocorreu um erro");
+              await Future.delayed(Duration(seconds: 1));
+              showInfo("Informativo");
+              await Future.delayed(Duration(seconds: 1));
+              showSuccess("Sucesso!");
+
+            },
+            width: 200,
+          ),
+          RoundedButton(
+            icon: Icons.abc,
+            onPressed: () {},
+          ),
         ],
       )),
     );
