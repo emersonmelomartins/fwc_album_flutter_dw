c8cd63f552dc2c4c8000767da18a354929f0a799
Emerson Melo Martins <emerson.melo@sap.trijay.com.br>
2024-01-16 22:18:13 -0300

    configurando dotenv

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:fwc_album_app/src/app.dart';
+import 'package:fwc_album_app/src/core/config/env/env.dart';
 
-void main() {
+void main() async {
+  await Env.instance.load();
+
   runApp(const App());
 }
