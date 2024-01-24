58e921ea3bcfab54864814e4bf2dc085fc92a23a
Emerson Melo Martins <emerson.melo@sap.trijay.com.br>
2024-01-24 19:41:53 -0300

    logout removendo chaves shared preferences

--- lib/src/pages/home/home_page.dart
+++ lib/src/pages/home/home_page.dart
@@ -1,12 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("Home"),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Home"),
+      ),
+      body: Center(
+        child: ElevatedButton(
+            onPressed: () async {
+              final sp = await SharedPreferences.getInstance();
+              sp.clear();
+              Navigator.of(context)
+                  .pushNamedAndRemoveUntil("/", (route) => false);
+            },
+            child: Text("Sair")),
+      ),
     );
   }
 }
