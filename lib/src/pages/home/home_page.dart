b8a4185d55ce1f6e2328a6d7db50ab772ff47a62
Emerson Melo Martins <emerson.melo@sap.trijay.com.br>
2024-01-31 21:45:29 -0300

    home page, widgets customizados

--- lib/src/pages/home/home_page.dart
+++ lib/src/pages/home/home_page.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_getit/flutter_getit.dart';
-import 'package:fwc_album_app/src/core/rest/custom_dio.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+import 'package:fwc_album_app/src/core/ui/styles/button_styles.dart';
+import 'package:fwc_album_app/src/core/ui/styles/colors_app.dart';
+import 'package:fwc_album_app/src/core/ui/styles/text_styles.dart';
+import 'package:fwc_album_app/src/core/ui/widgets/button.dart';
+import 'package:fwc_album_app/src/pages/home/widgets/status_tile.dart';
+import 'package:fwc_album_app/src/pages/home/widgets/sticker_percent_widget.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -9,26 +12,89 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: context.colors.primary,
       appBar: AppBar(
-        title: const Text("Home"),
+        elevation: 0,
+        backgroundColor: context.colors.primary,
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(
+              Icons.logout,
+              color: Colors.white,
+            ),
+          ),
+        ],
       ),
-      body: Center(
-        child: Column(
-          children: [
-            ElevatedButton(
-                onPressed: () async {
-                  context.get<CustomDio>().auth().get("/me");
-                },
-                child: Text("Testando Token")),
-            ElevatedButton(
-                onPressed: () async {
-                  final sp = await SharedPreferences.getInstance();
-                  sp.clear();
-                  Navigator.of(context)
-                      .pushNamedAndRemoveUntil("/", (route) => false);
-                },
-                child: Text("Sair")),
-          ],
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/images/background.png"),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: LayoutBuilder(
+          builder: (_, constraints) {
+            return ConstrainedBox(
+              constraints: BoxConstraints(
+                minHeight: constraints.maxHeight,
+              ),
+              child: Center(
+                child: SingleChildScrollView(
+                  child: Column(
+                    children: [
+                      Container(
+                        padding: const EdgeInsets.only(bottom: 35),
+                        width: MediaQuery.of(context).size.width,
+                        child: Image.asset(
+                          "assets/images/bola.png",
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      StickerPercentWidget(
+                        percent: 60,
+                      ),
+                      const SizedBox(height: 20),
+                      Text(
+                        "45 figurinhas",
+                        style: context.textStyles.titleWhite,
+                      ),
+                      const SizedBox(height: 20),
+                      StatusTile(
+                        icon: Image.asset("assets/images/all_icon.png"),
+                        label: "Todas",
+                        value: 45,
+                      ),
+                      const SizedBox(height: 20),
+                      StatusTile(
+                        icon: Image.asset("assets/images/missing_icon.png"),
+                        label: "Faltando",
+                        value: 500,
+                      ),
+                      const SizedBox(height: 20),
+                      StatusTile(
+                        icon: Image.asset("assets/images/repeated_icon.png"),
+                        label: "Repetidas",
+                        value: 30,
+                      ),
+                      const SizedBox(height: 20),
+                      Button(
+                        onPressed: () {},
+                        width: MediaQuery.of(context).size.width * .9,
+                        style: context.buttonStyles.yellowOutlineButton,
+                        labelStyle: context
+                            .textStyles.textSecondaryFontExtraBold
+                            .copyWith(
+                          color: context.colors.yellow,
+                        ),
+                        label: "Minhas Figurinhas",
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            );
+          },
         ),
       ),
     );
---
+++ lib/src/pages/home/widgets/status_tile.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:fwc_album_app/src/core/ui/styles/colors_app.dart';
+import 'package:fwc_album_app/src/core/ui/styles/text_styles.dart';
+
+class StatusTile extends StatelessWidget {
+  final Image icon;
+  final String label;
+  final int value;
+
+  const StatusTile({
+    super.key,
+    required this.icon,
+    required this.label,
+    required this.value,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: MediaQuery.of(context).size.width * .7,
+      child: ListTile(
+        leading: SizedBox(
+          width: 46,
+          child: CircleAvatar(
+            radius: 46,
+            backgroundColor: context.colors.grey,
+            child: icon,
+          ),
+        ),
+        title: Text(
+          label,
+          style: context.textStyles.textPrimaryFontRegular.copyWith(
+            color: Colors.white,
+          ),
+        ),
+        contentPadding: EdgeInsets.zero,
+        trailing: Text(
+          "$value",
+          style: context.textStyles.textPrimaryFontMedium.copyWith(
+            color: Colors.white,
+          ),
+        ),
+      ),
+    );
+  }
+}
